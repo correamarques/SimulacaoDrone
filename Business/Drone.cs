4141b704dcf8ea6953e812577223e8f6f41d675c
Fabian <fabian@correamarques.com>
2015-07-16 22:15:44 -0300

    Primeiro commit

---
+++ Business/Drone.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Business
+{
+    public class Drone
+    {
+		public static string Evaluate(string input)
+		{
+			if (!IsAlphanumeric(input))
+				return "(999, 999)";
+
+			//The input does not have X nor number
+			if (!HasX(input) && !HasNumber(input))
+				return SimpleSum(input);
+
+			if (HasX(input) && !HasNumber(input))
+				return SumWithX(input);
+
+
+			return HasX(input).ToString();
+		}
+
+		private static string SumWithX(string input)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		/// <summary>
+		/// Search on the input to find an invalid character
+		/// </summary>
+		/// <param name="input">Position</param>
+		/// <returns>True or false</returns>
+		private static bool IsAlphanumeric(string input)
+		{
+			return Regex.Matches(input, "^[A-Za-z0-9]+$").Count > 0;
+		}
+
+
+		static string SimpleSum(string input)
+		{// Filter chars on input
+			IEnumerable<char> charList = input.Distinct();
+			List<int> charCount = new List<int>();
+
+			foreach (char item in charList)
+			{
+				//Conta a quantidade de caracteres repetidos
+				int count = input.Count(i => i == item);
+				
+				// Sul ou Oeste deve ter posição negativa no gráfico
+				if (item == 'S' || item == 'O')
+					charCount.Add(count * -1);
+				else
+					charCount.Add(count);
+			}
+
+			return TheDronePointIs(charCount[0], charCount[1]);
+		}
+
+		static string TheDronePointIs(int positionX, int positionY)
+		{
+			return string.Format("({0}, {1})", positionX, positionY);
+		}
+
+		static bool HasX(string input)
+		{
+			return Regex.Matches(input, "[X|x]").Count > 0;
+		}
+		static bool HasNumber(string input)
+		{
+			return Regex.Matches(input, @"\d").Count > 0;
+		}
+    }
+}
---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using Business;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		#region Invalid tests
+		[TestMethod]
+		public void Input_Invalid()
+		{
+			Assert.AreEqual("(999, 999)", Drone.Evaluate("LLL$LNX2NNN"));
+		}
+		#endregion
+		
+		#region Simple tests
+		[TestMethod]
+		public void Input_LLLLNNNN()
+		{
+			Assert.AreEqual("(4, 4)", Drone.Evaluate("LLLLNNNN"));
+		}
+		[TestMethod]
+		public void Input_OOOSSSS()
+		{
+			Assert.AreEqual("(-3, -4)", Drone.Evaluate("OOOSSSS"));
+
+		}
+		[TestMethod]
+		public void Input_OOSOSSOSOOSSSS()
+		{
+			Assert.AreEqual("(-6, -8)", Drone.Evaluate("OOSOSSOSOOSSSS"));
+		}
+		#endregion
+
+		[TestMethod]
+		public void Input_OOXXSOSXSOSOOXXSSS()
+		{
+			Assert.AreEqual("(-2, -6)", Drone.Evaluate("OOXXSOSXSOSOOXXSSS"));
+		}
+		[TestMethod]
+		public void Input_OOXXSOSXSOSOOXXSSSX()
+		{
+			Assert.AreEqual("(-2, -5)", Drone.Evaluate("OOXXSOSXSOSOOXXSSSX"));
+		}
+
+	}
+}
